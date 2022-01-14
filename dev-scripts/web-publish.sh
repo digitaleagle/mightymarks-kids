235d3e25736d3bc0fa453c72219cd08f050a3371
Stephen Phillips <stephen.kent.phillips@email.com>
2022-01-14 16:25:08 -0500

    fixed build for website

--- dev-scripts/web-publish.sh
+++ dev-scripts/web-publish.sh
@@ -9,16 +9,16 @@ echo Flutter directory $(pwd)
 
 echo Publishing to $WEBSITE_HOST
 
-#flutter build web --base-href="/mightymarks/"
+flutter build web --base-href="/mightymarks/"
 cd build
-#if [ -e web.tar.gz ]; then
-#  echo "File exists"
-#  rm web.tar.gz
-#fi;
+if [ -e web.tar.gz ]; then
+  echo "File exists"
+  rm web.tar.gz
+fi;
 cd web
-#tar -czvf ../web.tar.gz *
+tar -czvf ../web.tar.gz *
 
-# scp ../web.tar.gz $WEBSITE_HOST:$SERVER_STAGING_PATH/website.tar.gz
+ scp ../web.tar.gz $WEBSITE_HOST:$SERVER_STAGING_PATH/website.tar.gz
 cd ../..
 scp dev-scripts/secrets.sh $WEBSITE_HOST:$SERVER_STAGING_PATH/
 scp dev-scripts/server-publish.sh $WEBSITE_HOST:$SERVER_STAGING_PATH/
